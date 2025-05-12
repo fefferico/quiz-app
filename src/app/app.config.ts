08e540880c4d8965670316a31250e211f5f2da60
fefferico <fefferico@gmail.com>
2025-05-13 01:54:38 +0200

    Add PWA features and configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 // src/app/app.config.ts
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter, TitleStrategy } from '@angular/router'; // Import TitleStrategy
 import { routes } from './app.routes';
 // import { HttpClientModule } from '@angular/common/http'; // If you need HttpClient later
@@ -7,12 +7,20 @@ import { routes } from './app.routes';
 // Optional: Custom Title Strategy
 import { FeedbackService } from './core/services/template-page-title-strategy.service';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker, ServiceWorkerModule } from '@angular/service-worker';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    importProvidersFrom(ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(), // Enable for production
+      registrationStrategy: 'registerWhenStable:30000'
+    }))
     // provideHttpClient(), // Example if you were to use HttpClient
     //{ provide: TitleStrategy, useClass: FeedbackService } // For dynamic page titles
     // DatabaseService is already providedIn: 'root'
